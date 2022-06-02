e8ace91d98b88afca8bb934052b5c09fe7b4ea4d
Logan Connolly <logan.connolly@protonmail.com>
2022-06-02 20:06:47 +0200

    feat(injection): add handler for http requests

--- injection/greet.go
+++ injection/greet.go
@@ -1,10 +1,21 @@
-package injection
+package main
 
 import (
 	"fmt"
 	"io"
+	"net/http"
+	"os"
 )
 
 func Greet(w io.Writer, name string) {
 	fmt.Fprintf(w, "Hello, %s", name)
+}
+
+func GreetHandler(rw http.ResponseWriter, r *http.Request) {
+	Greet(rw, "Internet")
+}
+
+func main() {
+	http.ListenAndServe(":8000", http.HandlerFunc(GreetHandler))
+	Greet(os.Stdout, "Console")
 }
