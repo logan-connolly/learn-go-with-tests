a6962ccbf1bb445e2bed96a1d73cc9fb33c19bb4
Logan Connolly <logan.connolly@protonmail.com>
2022-06-21 23:06:18 +0200

    refactor(context): pass context to store
    It shouldn't be the responsibility of the webserver
    to concern itself with the cancellation of the store.
    Better is to pass the context on as the first parameter
    of the `Fetch` function. So that it can decide when to
    cancel the routine.

--- internal/context/server.go
+++ internal/context/server.go
@@ -1,29 +1,25 @@
 package context
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		data := make(chan string, 1)
+		data, err := store.Fetch(r.Context())
 
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
+		if err != nil {
+			log.Printf("got error when fetching data: '%s'", err)
+			return
 		}
+
+		fmt.Fprint(w, data)
 	}
 }
--- internal/context/server_test.go
+++ internal/context/server_test.go
@@ -2,30 +2,19 @@ package context
 
 import (
 	"context"
+	"errors"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 )
 
-type SpyStore struct {
-	response  string
-	cancelled bool
-}
-
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
-}
-
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
-}
-
 func TestServer(t *testing.T) {
 	t.Run("server responds with the correct data", func(t *testing.T) {
 		data := "hello world"
-		svr := Server(&SpyStore{response: data})
+		store := &SpyStore{data}
+		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 		response := httptest.NewRecorder()
@@ -37,21 +26,71 @@ func TestServer(t *testing.T) {
 		}
 	})
 
-	t.Run("server cancels request via request context", func(t *testing.T) {
-		data := "hello world"
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
 		store := &SpyStore{response: data}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
-		cancellingCtx, cancel := context.WithCancel(request.Context())
-		time.AfterFunc(5*time.Millisecond, cancel)
-		request = request.WithContext(cancellingCtx)
-		response := httptest.NewRecorder()
+
+		ctx, cancel := context.WithTimeout(request.Context(), 5*time.Millisecond)
+		defer cancel()
+
+		request = request.WithContext(ctx)
+		response := &SpyResponseWriter{}
 
 		svr.ServeHTTP(response, request)
 
-		if !store.cancelled {
-			t.Error("expected the store to cancel")
+		if response.written {
+			t.Error("a response should not have been written")
 		}
 	})
+}
+
+type SpyStore struct {
+	response string
+}
+
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				log.Println("spy store got cancelled")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
+}
+
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
 }
