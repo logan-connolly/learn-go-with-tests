7059f336631dc44592c284cf2434fdf78f1e3dc4
Logan Connolly <logan.connolly@protonmail.com>
2022-06-28 16:53:51 +0200

    feat(pbt): add arabic to roman support

---
+++ internal/pbt/roman.go
@@ -0,0 +1,35 @@
+package pbt
+
+import "strings"
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+	return result.String()
+}
