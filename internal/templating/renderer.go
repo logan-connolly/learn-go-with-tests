cbd6ceac695b5f6fda74d853f9346c50b36627a8
Logan Connolly <logan.connolly@protonmail.com>
2023-01-26 18:28:33 +0100

    refactor(templating): use .tmpl extension for hl

--- internal/templating/renderer.go
+++ internal/templating/renderer.go
@@ -12,7 +12,7 @@ var blogTemplates embed.FS
 // Render takes a io.Writer and Post and inserts it into
 // the defined blog template.
 func Render(w io.Writer, p Post) error {
-	tmpl, err := template.ParseFS(blogTemplates, "templates/*.gohtml")
+	tmpl, err := template.ParseFS(blogTemplates, "templates/*.tmpl")
 	if err != nil {
 		return err
 	}
